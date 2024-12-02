d2d1e2b63c549574f18a1ffa388c82bb977803e4
aoloader <wkdaoloder@gmail.com>
2024-12-03 03:34:15 +0900

    First Cut Scene
    + Character Controller, Script add
    + Curtain, Script add
    = Yellow Brick Material Value change
    = FirstCutScene edit
    - Terrain Data delete

---
+++ Assets/Scripts/CurtainMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < 4f)
+        {
+            transform.Translate(Vector3.up * 0.75f * Time.deltaTime, Space.World);
+        }
+        else if (transform.position.y < 10f)
+        {
+            transform.Translate(Vector3.up * 0.75f * Time.deltaTime, Space.World);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, Quaternion.Euler(-15f, 0f, 0f), 0.3f * Time.deltaTime);
+        }
+        else
+        {
+            // SceneManager.LoadScene("Next Scene");
+        }
+    }
+}
